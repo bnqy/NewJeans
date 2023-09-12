c4b5b46174740532fbcdf56f66ea9f03271ad87f
Beneqi <obbonobo@gmail.com>
2023-09-12 17:59:37 +0600

    Basic Str

---
+++ SegizinchiBolum/WorkingWithTexts/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using static System.Console;
+
+string s1 = "Bishkek";
+string s2 = "R, o, Pe, EE, oo,wa, mE";
+string[] s3 = s2.Split(",");
+WriteLine($"The word {s1} has {s1.Length} chars");
+
+for (int i = 0; i < s1.Length; i++)
+{
+    WriteLine(s1[i]);
+}
+
+foreach (var s in s3)
+{
+    WriteLine(s);
+}
