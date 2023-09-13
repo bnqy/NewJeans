626e91d82e6b393ca8e0ca046ad7ed5336c6b0e8
Beneqi <obbonobo@gmail.com>
2023-09-13 14:25:44 +0600

    Spliting with Regex CSV

--- SegizinchiBolum/WorkingWithRegex/Program.cs
+++ SegizinchiBolum/WorkingWithRegex/Program.cs
@@ -14,3 +14,24 @@ else
 {
     WriteLine("Wrong");
 }
+WriteLine();
+
+// str with regex
+
+string names = "\"Monsters, Inc.\",\"I, Tonya\",\"Lock, Stock and Two Smoking Barrels\"";
+WriteLine($"Names: {names}");
+string[] namesArr = names.Split(',');
+WriteLine("Names splited with Split Method: ");
+foreach (string name in namesArr)
+{
+    WriteLine(name);
+}
+WriteLine();
+
+Regex namesCheck = new("(?:^|,)(?=[^\"]|(\")?)\"?((?(1)[^\"]*|[^,\"]*))\"?(?=,|$)");
+MatchCollection namesRegex = namesCheck.Matches(names);
+WriteLine("Spliting with Regex: ");
+foreach (Match name in namesRegex)
+{
+    WriteLine(name.Groups[2].Value);
+}
