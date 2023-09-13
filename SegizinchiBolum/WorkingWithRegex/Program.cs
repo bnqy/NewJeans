1591a9abd6516b3e1c65e14cf63c2296682a99c1
Beneqi <obbonobo@gmail.com>
2023-09-13 13:49:46 +0600

    Expressions Regex

--- SegizinchiBolum/WorkingWithRegex/Program.cs
+++ SegizinchiBolum/WorkingWithRegex/Program.cs
@@ -1,2 +1,16 @@
 ﻿using System.Text.RegularExpressions;
 using static System.Console;
+
+Write("Write your age: ");
+string? age = ReadLine();
+
+Regex ageChecker = new(@"^\d+$");
+
+if (ageChecker.IsMatch(age))
+{
+    WriteLine("GRAAAPE");
+}
+else
+{
+    WriteLine("Wrong");
+}
