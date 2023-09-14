8be1e125ec2f276bfb151ef8b2e9b5febdf137a7
Beneqi <obbonobo@gmail.com>
2023-09-14 21:51:13 +0600

    attributes

---
+++ SegizinchiBolum/WorkingWithReflections/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using static System.Console;
+
+WriteLine("Assembly metadata: ");
+Assembly? assembly = Assembly.GetEntryAssembly();
+if (assembly == null)
+{
+    WriteLine("Failed");
+}
+
+WriteLine($"Name: {assembly.FullName}");
+WriteLine($"Location: {assembly.Location}");
+
+IEnumerable<Attribute> attributes = assembly.GetCustomAttributes();
+WriteLine("Assembly-level attributes");
+foreach (Attribute attribute in attributes)
+{
+    WriteLine($" {attribute.GetType()}");
+}
