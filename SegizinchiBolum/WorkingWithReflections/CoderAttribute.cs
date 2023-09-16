4c4851e6d85314b429cbdae511b7a3413b83987e
Beneqi <obbonobo@gmail.com>
2023-09-16 15:55:38 +0600

    Error in Attributes

---
+++ SegizinchiBolum/WorkingWithReflections/CoderAttribute.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace WorkingWithReflections;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public class CoderAttribute : Attribute
+{
+    public string Coder { get; set; }
+    public DateTime LastModified { get; set; }
+    public CoderAttribute(string coder, string lastModified) 
+    { 
+        Coder = coder;
+        LastModified = DateTime.Parse(lastModified);
+    }
+
+
+}
--- SegizinchiBolum/WorkingWithReflections/Program.cs
+++ SegizinchiBolum/WorkingWithReflections/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
 using static System.Console;
+using WorkingWithReflections;
+using System.Runtime.CompilerServices;
 
 WriteLine("Assembly metadata: ");
 Assembly? assembly = Assembly.GetEntryAssembly();
@@ -24,3 +26,40 @@ WriteLine($"Version: {version?.InformationalVersion}");
 
 AssemblyCompanyAttribute? company = assembly.GetCustomAttribute<AssemblyCompanyAttribute>();
 WriteLine($"Company: {company?.Company}");
+
+
+WriteLine();
+
+WriteLine("Types: ");
+Type[] types = assembly.GetTypes();
+foreach (Type type in types)
+{
+    WriteLine();
+    WriteLine($"Type: {type.FullName}");
+    MemberInfo[] members = type.GetMembers();
+
+    foreach (MemberInfo member in members)
+    {
+        WriteLine($"{member.MemberType}: {member.Name} ({member.DeclaringType?.Name})");
+
+        IOrderedEnumerable<CoderAttribute> coders = member.GetCustomAttribute<CoderAttribute>().OrderByDescending(c => c.LastModified);
+
+        foreach (CoderAttribute coder in coders)
+        {
+            WriteLine($"-> Bodified by {coder.Coder} on {coder.LastModified.ToShortDateString()}");
+        }
+    }
+}
+class Animal
+{
+    [Coder("Beneq", "15 September 2023")]
+    [Coder("MainB", "23 September 2023")]
+
+    public void Speak()
+    {
+        WriteLine("Meow...");
+    }
+}
+
+
+
