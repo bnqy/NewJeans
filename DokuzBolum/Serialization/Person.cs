be8e4ccd326243a512a31c6f98268debbaf82970
Beneqi <obbonobo@gmail.com>
2023-09-21 15:43:20 +0600

    Serialization and Deserialization XML the thing

---
+++ DokuzBolum/Serialization/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace Serialization;
+using System.Xml.Serialization;
+
+public class Person
+{
+    public Person() { }
+    public Person(decimal initSalary)
+    {
+        Salary = initSalary;
+    }
+
+    [XmlAttribute("fname")]
+    public string? FirstName { get; set; }
+
+    [XmlAttribute("lname")]
+    public string? LastName { get; set; }
+
+    [XmlAttribute("dob")]
+    public DateTime DateOfBirth { get; set; }
+    public HashSet<Person>? Children { get; set; }
+    protected decimal Salary { get; set; }
+}
---
+++ DokuzBolum/Serialization/Program.cs
@@ -0,0 +1,69 @@
+﻿using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+using System.Xml.Serialization;
+using Serialization;
+
+
+List<Person> people = new()
+{
+    new(3000M)
+    {
+        FirstName = "Dua",
+        LastName = "Lipa",
+        DateOfBirth = new DateTime(1995, 07, 22)
+    },
+
+    new(1500M)
+    {
+        FirstName = "Jaeden",
+        LastName = "Martell",
+        DateOfBirth = new DateTime(2003, 01, 04)
+    },
+
+    new(4500M)
+    {
+        FirstName = "Kendall",
+        LastName = "Jenner",
+        DateOfBirth = new DateTime(1995, 07, 22),
+        Children = new()
+        {
+            new(0M)
+            {
+                FirstName = "Ocean",
+                LastName = "Jenner",
+                DateOfBirth = new(2023, 12, 16)
+            }
+        }
+    }
+
+};
+
+XmlSerializer xs = new(people.GetType());
+
+string path = Combine(CurrentDirectory, "peope.xml");
+
+using (FileStream stream = File.Create(path))
+{
+    xs.Serialize(stream, people);
+}
+
+WriteLine("Written {0:N0} bytes of XML to {1}",
+ arg0: new FileInfo(path).Length,
+ arg1: path);
+WriteLine();
+
+WriteLine(File.ReadAllText(path));
+
+using (FileStream fileLoad = File.Open(path, FileMode.Open))
+{
+    List<Person>? loadedPeople = xs.Deserialize(fileLoad) as List<Person>;
+
+    if (loadedPeople is not null)
+    {
+        foreach (Person p in loadedPeople)
+        {
+            WriteLine("{0} has {1} children.", p.LastName, p.Children?.Count ?? 0);
+        }
+    }
+}
