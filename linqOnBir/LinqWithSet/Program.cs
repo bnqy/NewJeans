aa1e7f700a16883ad4e8888e79cd80ea61be1224
Beneqi <obbonobo@gmail.com>
2023-10-11 14:51:52 +0600

    NaN

--- linqOnBir/LinqWithSet/Program.cs
+++ linqOnBir/LinqWithSet/Program.cs
@@ -14,8 +14,13 @@ Output(cohort3, "Cohort-3");
 Output(cohort3.Distinct(), "Cohort-3.Distinct()");
 Output(cohort3.DistinctBy(name => name.Substring(0, 2)), "cohort3.DistinctBy(name => name.Substring(0, 2))");
 Output(cohort2.Union(cohort3), "cohort2.Union(cohort3)");
-Output(cohort2.Zip(cohort3, (c2, c3) => $"{c2} with {c3}"));
+Output(cohort2.Zip(cohort3, (c2, c3) => $"{c2} with {c3}"), "cohort2.Zip(cohort3)");
 
+var v = double.NegativeInfinity;
+var v2 = double.NaN;
+var v3 = double.PositiveInfinity;
+var v4 = double.Epsilon;
+Console.WriteLine($"NegativeInfinity: {v}\nNaN: {v2}\nPositiveInfinity: {v3}\nEpsilon: {v4}");
 
 static void Output(IEnumerable<string> cohort, string description = "")
 {
