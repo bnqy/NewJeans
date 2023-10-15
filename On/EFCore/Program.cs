1356c6cfd1d4d301f9823cbf10691ba0a32a7dbd
Beneqi <obbonobo@gmail.com>
2023-10-15 14:46:06 +0600

    filtered includes

--- On/EFCore/Program.cs
+++ On/EFCore/Program.cs
@@ -4,8 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 WriteLine($"Using \"{ProjectConstants.DatabaseProvider}\" provider");
 
-QueryingCategories();
-
+//QueryingCategories();
+FilteredIncludes();
 static void QueryingCategories()
 {
     using (Northwind db = new())
@@ -25,4 +25,30 @@ static void QueryingCategories()
             WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
         }
     }
+}
+
+static void FilteredIncludes()
+{
+    using (Northwind db = new())
+    {
+        Write("Enter a minimum for unit in stoks: ");
+        string stockString = ReadLine() ?? "10";
+        int stock = int.Parse( stockString );
+
+        IQueryable<Category>? categories = db.Categories?.Include(c => c.Products.Where(p => p.Stock >= stock));
+
+        if (categories is null)
+        {
+            WriteLine("No categories found.");
+            return;
+        }
+        foreach (Category c in categories)
+        {
+            WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of {stock} units in stock.");
+            foreach (Product p in c.Products)
+            {
+                WriteLine($" {p.ProductName} has {p.Stock} units in stock.");
+            }
+        }
+}
 }
