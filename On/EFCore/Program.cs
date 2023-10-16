16cd94f551a1d2db7a71876dc67a4de311629a8c
Beneqi <obbonobo@gmail.com>
2023-10-16 21:23:48 +0600

    the end

--- On/EFCore/Program.cs
+++ On/EFCore/Program.cs
@@ -1,15 +1,21 @@
 ﻿using static System.Console;
 using EFCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 WriteLine($"Using \"{ProjectConstants.DatabaseProvider}\" provider");
 
 //QueryingCategories();
-FilteredIncludes();
+//FilteredIncludes();
+QueryingProducts();
 static void QueryingCategories()
 {
     using (Northwind db = new())
     {
+        ILoggerFactory loggerFactory = db.GetService<ILoggerFactory>();
+        loggerFactory.AddProvider(new ConsoleLoggerProvider());
         WriteLine("Categories and how many products they have:");
         
         IQueryable<Category>? categories = db.Categories?.Include(c => c.Products);
@@ -42,6 +48,7 @@ static void FilteredIncludes()
             WriteLine("No categories found.");
             return;
         }
+        WriteLine($"ToQueryString: {categories.ToQueryString()}");
         foreach (Category c in categories)
         {
             WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of {stock} units in stock.");
@@ -51,4 +58,37 @@ static void FilteredIncludes()
             }
         }
 }
+}
+
+static void QueryingProducts()
+{
+    using (Northwind db = new())
+    {
+        ILoggerFactory loggerFactory = db.GetService<ILoggerFactory>();
+        loggerFactory.AddProvider(new ConsoleLoggerProvider());
+        WriteLine("Products that cost more than a price, highest at top.");
+        string? input;
+        decimal price;
+
+        do
+        {
+            Write("Enter a product price: ");
+            input = ReadLine();
+        } while (!decimal.TryParse(input, out price));
+
+        IQueryable<Product>? products = db.Products?
+            .Where(product => product.Cost > price)
+            .OrderByDescending(product => product.Cost);
+
+        if (products is null)
+        {
+            WriteLine("No products found.");
+            return;
+        }
+        foreach (Product p in products)
+        {
+            WriteLine("{0}: {1} costs {2:$#,##0.00} and has {3} in stock.",
+            p.ProductId, p.ProductName, p.Cost, p.Stock);
+        }
+    }
 }
