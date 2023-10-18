1c81394e3a112335d4aaa6a4da83cb898463ecdf
Beneqi <obbonobo@gmail.com>
2023-10-18 21:51:24 +0600

    Aggregate methods

--- linqOnBir/LinqEFCore/Program.cs
+++ linqOnBir/LinqEFCore/Program.cs
@@ -3,7 +3,9 @@ using LinqEFCore;
 using Microsoft.EntityFrameworkCore;
 
 
-FilterAndSort();
+//FilterAndSort();
+//JoinCategoriesAndProducts();
+GroupJoinCategoriesAndProducts();
 
 static void FilterAndSort()
 {
@@ -17,12 +19,77 @@ static void FilterAndSort()
         IOrderedQueryable<Product> filteredAndSortedProducts =
             filteredProducts.OrderByDescending(product => product.UnitPrice);
 
+
+        var projectedProducts = filteredAndSortedProducts
+            .Select(product => new 
+            {
+                product.ProductId,
+                product.ProductName,
+                product.UnitPrice
+            });
+
         WriteLine("Products that cost less than $10:");
-        foreach (Product p in filteredAndSortedProducts)
+
+        foreach (var p in projectedProducts)
         {
             WriteLine("{0}: {1} costs {2:$#,##0.00}",
             p.ProductId, p.ProductName, p.UnitPrice);
         }
+
+        /*foreach (Product p in filteredAndSortedProducts)
+        {           
+            WriteLine("{0}: {1} costs {2:$#,##0.00}",
+            p.ProductId, p.ProductName, p.UnitPrice);
+        }*/
         WriteLine();
+    }
+}
+
+static void JoinCategoriesAndProducts()
+{
+    using (Northwind db = new())
+    {
+        var queryJoin = db.Categories.Join(
+        inner: db.Products,
+        outerKeySelector: category => category.CategoryId,
+        innerKeySelector: product => product.CategoryId,
+        resultSelector: (c, p) =>
+        new { c.CategoryName, p.ProductName, p.ProductId })
+            .OrderBy(cp => cp.CategoryName);
+
+        foreach (var item in queryJoin)
+        {
+            WriteLine("{0}: {1} is in {2}.",
+            arg0: item.ProductId,
+            arg1: item.ProductName,
+            arg2: item.CategoryName);
+        }
+    }
+}
+
+static void GroupJoinCategoriesAndProducts()
+{
+    using (Northwind db = new())
+    {
+        var queryGroup = db.Categories.AsEnumerable().GroupJoin(
+        inner: db.Products,
+        outerKeySelector: category => category.CategoryId,
+        innerKeySelector: product => product.CategoryId,
+        resultSelector: (c, matchingProducts) => new
+        {
+            c.CategoryName,
+            Products = matchingProducts.OrderBy(p => p.ProductName)
+        });
+
+        foreach (var category in queryGroup)
+        {
+            WriteLine("{0} has {1} products.",
+            arg0: category.CategoryName,
+            arg1: category.Products.Count());
+            foreach (var product in category.Products)
+            {
+                WriteLine($" {product.ProductName}");
+            }
+        }
     }
 }
