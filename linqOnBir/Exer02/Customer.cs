55b502940d57076a25e20df65f53644b932d2df4
Beneqi <obbonobo@gmail.com>
2023-10-26 12:31:02 +0600

    Exer02 created

---
+++ linqOnBir/Exer02/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exer02;
+
+public class Customer
+{
+
+    [StringLength(5)]
+    public string? CustomerId { get; set; }
+
+    [Required]
+    [StringLength(40)]
+    public string? CompanyName { get; set; }
+
+    [StringLength(30)]
+    public string? ContactName { get; set; }
+
+}
