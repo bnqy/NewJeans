e6a01f6d81fd2a4be1f1253b4cdd5496ea96a83a
Beneqi <obbonobo@gmail.com>
2023-11-29 19:07:00 +0600

    gpaph implementing

---
+++ Practices/Practices/Graph.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+namespace Practices;
+
+public class Graph
+{
+	private Dictionary<string, List<string>> graph;
+
+	public Graph()
+	{
+		graph = new Dictionary<string, List<string>>();
+	}
+
+	public void AddNode(string node)
+	{
+		if (!graph.ContainsKey(node))
+		{
+			graph[node] = new List<string>();
+		}
+	}
+
+	public void AddEdges(string node1, string node2)
+	{
+		if (graph.ContainsKey(node1) && graph.ContainsKey(node2))
+		{
+			graph[node1].Add(node2);
+			graph[node2].Add(node1);
+		}
+	}
+
+	public void Display()
+	{
+		foreach (var node in graph)
+		{
+			WriteLine($"Node: {node.Key}");
+			WriteLine($" {string.Join(", ", node.Value)}");
+		}
+	}
+}
---
+++ Practices/Practices/Program.cs
@@ -0,0 +1,16 @@
+﻿using Practices;
+using static System.Console;
+
+Graph graph = new Graph();
+
+graph.AddNode("A");
+graph.AddNode("B");
+graph.AddNode("C");
+graph.AddNode("D");
+
+graph.AddEdges("A", "B");
+graph.AddEdges("B", "C");
+graph.AddEdges("C", "D");
+graph.AddEdges("D", "A");
+
+graph.Display();
